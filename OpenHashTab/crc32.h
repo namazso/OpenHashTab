1bec8f3906e7ed4b39530f05e52f8379035539c7
namazso <xynmzs@gmail.com>
2020-02-06 12:34:19 +0100

    Add CRC32 implementation

---
+++ OpenHashTab/crc32.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef EXTERN_C_START
+#ifdef __cplusplus
+#define EXTERN_C_START extern "C" {
+#define EXTERN_C_END }
+#else
+#define EXTERN_C_START
+#define EXTERN_C_END
+#endif
+#endif
+
+EXTERN_C_START
+
+#include <stddef.h>
+#include <stdint.h>
+
+extern uint32_t Crc32_ComputeBuf(uint32_t inCrc32, const void* buf, size_t bufLen);
+
+EXTERN_C_END
