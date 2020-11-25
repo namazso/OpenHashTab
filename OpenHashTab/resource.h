fab610669fd0c50afeed9526c0035bd35ba66533
namazso <8676443+namazso@users.noreply.github.com>
2020-11-25 01:03:09 +0100

    add an icon

--- OpenHashTab/resource.h
+++ OpenHashTab/resource.h
@@ -35,12 +35,13 @@
 #define IDS_NOMATCH                     218
 #define IDS_ERROR                       219
 #define IDS_CANCEL                      220
+#define IDI_ICON1                       221
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        221
+#define _APS_NEXT_RESOURCE_VALUE        222
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         217
 #define _APS_NEXT_SYMED_VALUE           107
