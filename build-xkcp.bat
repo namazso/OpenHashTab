41554663c8ada3617d2fea34cb8f76c356adc1c8
namazso <admin@namazso.eu>
2021-04-27 14:07:03 +0200

    msys2 -> wslv1

--- build-xkcp.bat
+++ build-xkcp.bat
@@ -1,11 +1,11 @@
 mkdir obj
 set CHERE_INVOKING=1 || exit /B 1
-c:\msys64\usr\bin\bash -l -e -c "cd XKCP && make generic32/libXKCP.a.vcxproj" || exit /B 1
-c:\msys64\usr\bin\bash -l -e -c "cd XKCP && make generic64/libXKCP.a.vcxproj" || exit /B 1
-c:\msys64\usr\bin\bash -l -e -c "cd XKCP && make AVX/libXKCP.a.vcxproj" || exit /B 1
-c:\msys64\usr\bin\bash -l -e -c "cd XKCP && make AVX2noAsm/libXKCP.a.vcxproj" || exit /B 1
-c:\msys64\usr\bin\bash -l -e -c "cd XKCP && make AVX512noAsm/libXKCP.a.vcxproj" || exit /B 1
-c:\msys64\usr\bin\bash -l -e -c "cd XKCP && make ARMv8A/libXKCP.a.vcxproj" || exit /B 1
+wsl -d Alpine -- /bin/ash -e -c "cd XKCP && make generic32/libXKCP.a.vcxproj" || exit /B 1
+wsl -d Alpine -- /bin/ash -e -c "cd XKCP && make generic64/libXKCP.a.vcxproj" || exit /B 1
+wsl -d Alpine -- /bin/ash -e -c "cd XKCP && make AVX/libXKCP.a.vcxproj" || exit /B 1
+wsl -d Alpine -- /bin/ash -e -c "cd XKCP && make AVX2noAsm/libXKCP.a.vcxproj" || exit /B 1
+wsl -d Alpine -- /bin/ash -e -c "cd XKCP && make AVX512noAsm/libXKCP.a.vcxproj" || exit /B 1
+wsl -d Alpine -- /bin/ash -e -c "cd XKCP && make ARMv8A/libXKCP.a.vcxproj" || exit /B 1
 
 msbuild -p:Configuration=Release -p:PlatformToolset=ClangCL -p:CharacterSet=Unicode -p:FunctionLevelLinking=false -p:WholeProgramOptimization=false -p:RuntimeLibrary=MultiThreaded -p:"OutDir=$(ProjectDir)..\..\..\..\..\obj\\" -p:Platform=Win32 -p:TargetName=libXKCP-Win32-sse2 XKCP\bin\VC\generic32_libXKCP.a.vcxproj || exit /B 1
 msbuild -p:Configuration=Release -p:PlatformToolset=ClangCL -p:CharacterSet=Unicode -p:FunctionLevelLinking=false -p:WholeProgramOptimization=false -p:RuntimeLibrary=MultiThreaded -p:"OutDir=$(ProjectDir)..\..\..\..\..\obj\\" -p:Platform=Win32 -p:TargetName=libXKCP-Win32-avx XKCP\bin\VC\AVX_libXKCP.a.vcxproj || exit /B 1
