678e48129809133ea782e8ad5369aebfa0248d52
namazso <admin@namazso.eu>
2022-03-09 12:29:28 +0100

    add standalone runner

---
+++ StandaloneStub/StandaloneStub.cpp
@@ -0,0 +1,21 @@
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <Windows.h>
+
+extern "C" __declspec(dllimport) int APIENTRY StandaloneEntryW(
+  _In_opt_  HWND      hWnd,
+  _In_      HINSTANCE hRunDLLInstance,
+  _In_      LPCWSTR   lpCmdLine,
+  _In_      int       nShowCmd
+);
+
+int APIENTRY wWinMain(
+  _In_ HINSTANCE hInstance,
+  _In_opt_ HINSTANCE hPrevInstance,
+  _In_ LPWSTR lpCmdLine,
+  _In_ int nCmdShow
+)
+{
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  return StandaloneEntryW(nullptr, hInstance, lpCmdLine, nCmdShow);
+}
